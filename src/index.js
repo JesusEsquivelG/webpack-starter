dd2475fcbb350886cfecced3ef2dcce9cf8e51bc
jesquivel <jesqgo@gmail.com>
2022-04-16 15:24:09 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import { saludar } from './js/componentes.js'
+import { suma } from './js/componentes.js'
+
+
+const nombre = 'Jesus'
+saludar(nombre);
+console.log(suma(2,8));
